f79f49fa898a2e8a1769a4c033fa9ce332cd7ef1
Octanoz <130052848+Octanoz@users.noreply.github.com>
2025-07-31 16:34:01 +0200

    Added static class to house all conditionally set colours.

---
+++ WordleClone2/Components/Model/WordleColors.cs
@@ -0,0 +1,14 @@
+﻿namespace WordleClone2.Components.Model;
+
+public static class WordleColors
+{
+    //Colors used in the guess lines
+    public const string GreyCell = "--grey";
+    public const string YellowCell = "--yellow";
+    public const string GreenCell = "--green";
+
+    //Colors used in the virtual keyboard
+    public const string BlackKey = "black-key";
+    public const string YellowKey = "yellow-key";
+    public const string GreenKey = "green-key";
+}
